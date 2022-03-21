943a959c4dd8d001959eeacb04c6dce612deef25
namdoi <doidangnam058@gmail.com>
2022-03-21 10:40:59 +0700

    Practice mod_rewrite

---
+++ public_html/display_article.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if (isset($_GET['articleId']) && $_GET['articleId'] = 'my-article') { ?>
+        <p>This is my-article</p>
+    <?php } ?> 
+    
+</body>
+</html>
